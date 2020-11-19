511c4d11b9d85c27caccb446af49e6733ef5bc72
Francisca <fvargasserani@gmail.com>
2020-11-19 14:09:34 -0300

    changed logic model

--- prueba.sql
+++ prueba.sql
@@ -43,3 +43,73 @@ CREATE TABLE factura_producto(
     FOREIGN KEY (id_factura) REFERENCES factura (id_factura),
     FOREIGN KEY (id_producto) REFERENCES producto (id_producto)
 );
+
+INSERT INTO cliente(
+    id_cliente,
+    nombre,
+    rut,
+    direccion
+) VALUES(
+    1,
+    'Roberto Jimenez',
+    '22.456.355-K',
+    'Av. Las Perdices 456, dpto 506, Penalolen'
+);
+
+INSERT INTO cliente(
+    id_cliente,
+    nombre,
+    rut,
+    direccion
+) VALUES(
+    2,
+    'Carina Gonzalez',
+    '16.356.494-9',
+    'Huerfanos 756, oficina 41, Santiago Centro'
+);
+
+INSERT INTO cliente(
+    id_cliente,
+    nombre,
+    rut,
+    direccion
+) VALUES(
+    3,
+    'Rosario Vargas',
+    '7.829.390-2',
+    'Holanda 4522, Providencia'
+);
+
+INSERT INTO cliente(
+    id_cliente,
+    nombre,
+    rut,
+    direccion
+) VALUES(
+    4,
+    'Pablo Perez',
+    '11.204.376-0',
+    'Av. Loreto 332, Recoleta'
+);
+
+INSERT INTO cliente(
+    id_cliente,
+    nombre,
+    rut,
+    direccion
+) VALUES(
+    5,
+    'Juan Ignacio Valdes',
+    '9.228.394-8',
+    'Principe de Gales 2282, La Reina'
+);
+
+INSERT INTO producto(
+    id_producto,
+    nombre_producto,
+    descripcion_producto,
+    valor_unitario,
+    id_categoria_producto
+) VALUES(
+    1
+)
